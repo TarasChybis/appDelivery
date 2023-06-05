24e9c48866c98e8b28c683dd8216d238c5cb99b4
Taras Chybis <t0660804676@gmail.com>
2023-06-06 00:40:52 +0300

    second page was added

---
+++ js/appDelivery.js
@@ -0,0 +1,51 @@
+var goods = {
+   "11111" : {
+      "name"   : "Burger with beef",
+      "cost"   : 120.00,
+      "img"    : "../img/Burger with beef.jpg"
+   },
+
+   "22222" : {
+      "name"   : "Golden French Fries",
+      "cost"   : 50.00,
+      "img"    : "../img/Golden French Fries.jpg"
+   },
+
+   "33333" : {
+      "name"   : "Mushroom cream-soup",
+      "cost"   : 90.00,
+      "img"    : "../img/Mushroom cream-soup.jpg"
+   },
+
+   "44444" : {
+      "name"   : "Penne with chicken and mushrooms",
+      "cost"   : 150.00,
+      "img"    : "../img/Penne with chicken and mushrooms.jpg"
+   },
+
+   "55555" : {
+      "name"   : "Perfect Croissants",
+      "cost"   : 150.00,
+      "img"    : "../img/Perfect Croissants.jpg"
+   },
+
+   "66666" : {
+      "name"   : "Pizza Enchanting meat",
+      "cost"   : 150.00,
+      "img"    : "../img/Pizza Enchanting meat.jpg"
+   },
+
+   "77777" : {
+      "name"   : "Spaghetti a La Carbonara with ham",
+      "cost"   : 130.00,
+      "img"    : "../img/Spaghetti a La Carbonara with ham.jpg"
+   },
+};
+
+console.log(goods);
+
+var out = "";
+
+for (var key in goods) {
+   
+} 
